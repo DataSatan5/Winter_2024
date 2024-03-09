48c16cc22c58eac86927dfe3a507e61493333163
Serge K <sergek2000@woz.com>
2024-03-09 14:47:36 +0600

    Исправлено задание 19-2

--- Task_19-2.py
+++ Task_19-2.py
@@ -5,8 +5,12 @@ class Fibonacci:
     def __iter__(self):
         return self
     def __next__(self):
-        self.a, self.b = self.b, self.a + self.b
+        #self.a, self.b = self.b, self.a + self.b
+        tmp = self.a + self.b
+        self.a = self.b
+        self.b = tmp
         return self.a
+
 x = Fibonacci()
 for _ in range(15):
     print(next(x), end=' ')
